0585a06f1091f4811c05fe0b5ab304a1fbf93a5b
ondrejgr <dev@gratz.cz>
2015-01-24 20:14:24 +0100

    initial empty project created

---
+++ slnLatrunculi/LatrunculiConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LatrunculiConsole
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
