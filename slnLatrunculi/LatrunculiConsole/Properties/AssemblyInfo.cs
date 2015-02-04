beaa555486546dd54b87b406345b330e5392230f
ondrejgr <dev@gratz.cz>
2015-02-04 11:59:29 +0100

    init vs2013

---
+++ slnLatrunculi/LatrunculiConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LatrunculiConsole")]
+[assembly: AssemblyDescription("Latrunculi")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("INF UPOL CZ")]
+[assembly: AssemblyProduct("Latrunculi")]
+[assembly: AssemblyCopyright("Copyright © Ondřej Grätz 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("79875fe4-b721-48ca-8670-9a8cf0f31153")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
